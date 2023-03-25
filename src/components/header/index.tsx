c60d2cea48a66138fe18fdb2dcf8c5447a94fcd3
giovani <giovaniocan@gmail.com>
2023-03-25 14:08:12 -0300

    CSS do header está pronto Header css is ready

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,7 @@ export function Header() {
       <h1>Devfinder</h1>
       <ThemeMode>
         <h2>dark</h2>
-        <Moon size={32} color="#fff" />
+        <Moon size={27} color="#fff" />
       </ThemeMode>
     </HeaderContainer>
   )
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -7,4 +7,16 @@ export const HeaderContainer = styled.div`
   justify-content: space-between;
 `
 
-export const ThemeMode = styled.div``
+export const ThemeMode = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.75rem;
+
+  h2 {
+    font-size: 2rem;
+    font-weight: 400;
+    font-variant: small-caps; // deixar tudo em maiusculo
+    letter-spacing: 4px; // espaçamento entre as letras
+  }
+`
