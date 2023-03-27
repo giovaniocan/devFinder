dcde8454472a3366f14eefc7e9418a69acec9b59
giovani <giovaniocan@gmail.com>
2023-03-27 16:20:21 -0300

    DevCard with static info and a little bit of css

--- src/components/devCard/index.tsx
+++ src/components/devCard/index.tsx
@@ -1,10 +1,61 @@
-import { DevCardContainer } from './styles'
+import {
+  DevCardContainer,
+  InfoContainer,
+  HeaderInfo,
+  TopInfo,
+  MiddleInfo,
+  Each,
+  BottomInfo,
+  EachBottomInfo,
+} from './styles'
+import { MapPin, Link, Buildings, TwitchLogo } from 'phosphor-react'
 
 export function DevCard() {
   return (
     <DevCardContainer>
-      teste
-      <h1>não sei</h1>
+      <img src="https://github.com/giovaniocan.png" alt="" />
+      <InfoContainer>
+        <TopInfo>
+          <HeaderInfo>
+            <h2>Giovani Apolinario Ocan</h2>
+            <span> Joined Oct 14, 2021</span>
+          </HeaderInfo>
+          <h5>@giovaniocan</h5>
+          <span>bio da pessoa</span>
+        </TopInfo>
+        <MiddleInfo>
+          <Each>
+            Repos
+            <h2>9</h2>
+          </Each>
+          <Each>
+            Followers
+            <h2>0</h2>
+          </Each>
+          <Each>
+            Following
+            <h2>0</h2>
+          </Each>
+        </MiddleInfo>
+        <BottomInfo>
+          <EachBottomInfo>
+            <MapPin size={25} />
+            Brasil
+          </EachBottomInfo>
+          <EachBottomInfo>
+            <TwitchLogo size={25} />
+            Not Available
+          </EachBottomInfo>
+          <EachBottomInfo>
+            <Link size={25} />
+            link do portifolio
+          </EachBottomInfo>
+          <EachBottomInfo>
+            <Buildings size={25} />
+            IFPR
+          </EachBottomInfo>
+        </BottomInfo>
+      </InfoContainer>
     </DevCardContainer>
   )
 }
--- src/components/devCard/styles.ts
+++ src/components/devCard/styles.ts
@@ -1,9 +1,62 @@
 import styled from 'styled-components'
 
 export const DevCardContainer = styled.div`
+  display: grid;
+  grid-template-columns: 10rem auto;
+  grid-gap: 1rem;
   margin-top: 2rem;
   width: 100%;
   min-height: 400px;
   background: ${(props) => props.theme.colors.baseBackgroundComponent};
   border-radius: 12px;
+
+  padding: 3rem;
+
+  img {
+    border-radius: 50%;
+    width: 9rem;
+    height: 9rem;
+  }
 `
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 1.5rem;
+`
+
+export const TopInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.7rem;
+
+  span {
+    color: ${(props) => props.theme.colors.textinput};
+  }
+
+  h5 {
+    color: ${(props) => props.theme.colors.searchComponent};
+    font-size: 1rem;
+    font-weight: 400;
+  }
+`
+
+export const HeaderInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+export const MiddleInfo = styled.div`
+  background: ${(props) => props.theme.colors.background};
+  border-radius: 12px;
+  display: flex;
+  padding: 1.5rem ;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Each = styled.div``
+
+export const BottomInfo = styled.div``
+
+export const EachBottomInfo = styled.div``
