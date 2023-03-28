54093367c30dd65b06c3675d0ec88d93e3f90aff
giovani <giovaniocan@gmail.com>
2023-03-28 13:05:34 -0300

    getting the user that was entered

--- src/components/searchProfile/index.tsx
+++ src/components/searchProfile/index.tsx
@@ -1,14 +1,31 @@
 import { SearchContainer, MagnifyingGlassButton, SearchButton } from './styles'
 import { MagnifyingGlass } from 'phosphor-react'
+import { ChangeEvent, useState } from 'react'
 
 export function SearchProfile() {
+  const [search, setSearch] = useState('')
+
+  function handleNewSearch(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('')
+    setSearch(event.target.value)
+  }
+
+  function handleCreatSearch() {
+    event?.preventDefault()
+    console.log(search)
+    setSearch(' ')
+  }
   return (
-    <SearchContainer>
+    <SearchContainer onSubmit={handleCreatSearch}>
       <MagnifyingGlassButton>
         <MagnifyingGlass size={40} color="#0d71ff" />
       </MagnifyingGlassButton>
 
-      <input type="text" placeholder="Search GitHub Username" />
+      <input
+        type="text"
+        placeholder="Search GitHub Username"
+        onChange={handleNewSearch}
+      />
       <SearchButton>Search</SearchButton>
     </SearchContainer>
   )
